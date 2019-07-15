b17dfd45d39a6f0c4010459fdf64f10ee5601c13
manuelernesto <manuelernesto@outlook.com>
2019-07-15 20:28:18 +0100

    home with navigation architecture

--- app/src/main/java/io/github/manuelernesto/mvvmapp/ui/home/HomeActivity.kt
+++ app/src/main/java/io/github/manuelernesto/mvvmapp/ui/home/HomeActivity.kt
@@ -2,12 +2,36 @@ package io.github.manuelernesto.mvvmapp.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import io.github.manuelernesto.mvvmapp.R
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var mNavController: NavController
+    private lateinit var mDrawer: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        setSupportActionBar(toolbar)
+
+
+        mNavController = Navigation.findNavController(this, R.id.fragment)
+        mDrawer = findViewById(R.id.drawer_layout)
+
+        NavigationUI.setupWithNavController(nav_view, mNavController)
+        NavigationUI.setupActionBarWithNavController(this, mNavController, mDrawer)
+
     }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(mNavController, mDrawer)
+    }
+
+
 }
---
+++ app/src/main/java/io/github/manuelernesto/mvvmapp/ui/home/profile/ProfileViewModel.kt
@@ -0,0 +1,7 @@
+package io.github.manuelernesto.mvvmapp.ui.home.profile
+
+import androidx.lifecycle.ViewModel;
+
+class ProfileViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
